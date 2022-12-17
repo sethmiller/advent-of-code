4e669d3fa5bd489648254350b85da2aa3ebea72d
Seth Miller <miller.seth@gmail.com>
2022-12-17 15:54:41 -0600

    Day 15: Part 2

--- day-15/main.go
+++ day-15/main.go
@@ -31,6 +31,11 @@ type Beacon struct {
 	sensors []*Sensor
 }
 
+type Pair struct {
+	start int
+	end   int
+}
+
 func max(a, b int) int {
 	if a > b {
 		return a
@@ -47,41 +52,58 @@ func min(a, b int) int {
 	return b
 }
 
-const target = 2_000_000
+const lower = 0
+const upper = 4_000_000
 
-func check(s *Sensor) *[2]int {
+func check(s *Sensor, target int) *Pair {
 	d := max(s.row, target) - min(s.row, target)
 	if d <= s.distance {
 		width := ((s.distance - d) * 2) + 1
-		fmt.Printf("  sr(%d) tr(%d) sd(%d) d(%d) w(%d)\n", s.row, target, s.distance, d, width)
+		// fmt.Printf("  sr(%d) tr(%d) sd(%d) d(%d) w(%d)\n", s.row, target, s.distance, d, width)
 		half := (width - 1) / 2
 		start := s.column - half
 		end := s.column + half
-		return &[2]int{start, end}
+		// Bound the result by range within which we are searching
+		pair := &Pair{start: max(lower, start), end: min(end, upper)}
+		if start > upper || end < lower {
+			// We intersected with the line outside of the range within which we are searching
+			return nil
+		}
+
+		return pair
 	}
 
 	return nil
 }
 
-func overlap(l1 *[2]int, l2 *[2]int) *[2]int {
+func overlap(l1 *Pair, l2 *Pair) *Pair {
 	// l1 start is inside l2
-	if l1[0] >= l2[0] && l1[0] <= l2[1] {
+	if l1.start >= l2.start && l1.start <= l2.end {
 		// is fully contained
-		if l1[1] <= l2[1] {
+		if l1.end <= l2.end {
 			return l2
 		}
 
-		return &[2]int{l2[0], l1[1]}
+		return &Pair{start: l2.start, end: l1.end}
 	}
 
-	// l2 start inside l1
-	if l2[0] >= l1[0] && l2[0] <= l1[1] {
+	// l2 start is inside l1
+	if l2.start >= l1.start && l2.start <= l1.end {
 		// is fully contained
-		if l2[1] <= l1[1] {
+		if l2.end <= l1.end {
 			return l1
 		}
 
-		return &[2]int{l1[0], l2[1]}
+		return &Pair{start: l1.start, end: l2.end}
+	}
+
+	// Adjacent
+	if l1.end+1 == l2.start {
+		return &Pair{start: l1.start, end: l2.end}
+	}
+
+	if l2.end+1 == l1.start {
+		return &Pair{start: l2.start, end: l1.end}
 	}
 
 	// No overlap
@@ -147,31 +169,50 @@ func main() {
 		panic(err)
 	}
 
-	lines := []*[2]int{}
-	for _, s := range sensors {
-		if line := check(s); line != nil {
-			l := 0
-			for l < len(lines) {
-				existing := lines[l]
-				if joined := overlap(line, existing); joined != nil {
-					// This line overlaps another. Remove the existing line.
-					lines = append(lines[:l], lines[l+1:]...)
-					// and update the line with the joined lines
-					line = joined
-				} else {
-					l++
+	lines := []*Pair{}
+outer:
+	for r := lower; r <= upper; r++ {
+		for _, s := range sensors {
+			if line := check(s, r); line != nil {
+				l := 0
+				for l < len(lines) {
+					existing := lines[l]
+					if joined := overlap(line, existing); joined != nil {
+						// This line overlaps another. Remove the existing line.
+						lines = append(lines[:l], lines[l+1:]...)
+						// and update the line with the joined lines
+						line = joined
+					} else {
+						l++
+					}
+				}
+				lines = append(lines, line)
+
+				if len(lines) == 1 && lines[0].start == lower && lines[0].end == upper {
+					// fmt.Printf("Row (%d) is full\n", r)
+					break
 				}
 			}
-			lines = append(lines, line)
 		}
+
+		switch len(lines) {
+		case 0:
+			panic(fmt.Sprintf("Oh no (%d)\n", r))
+		case 1:
+			// if lines[0].start == lower && lines[0].end == upper {
+			// 	fmt.Printf("Row (%d) is full\n", r)
+			// }
+		case 2:
+			fmt.Printf("It's probably (%d)\n", r)
+			for _, line := range lines {
+				fmt.Printf("  (%d, %d)\n", line.start, line.end)
+			}
+			fmt.Printf("Answer: %d\n", r+((lines[0].end+1)*4_000_000))
+			break outer
+		default:
+			panic(fmt.Sprintf("Weird (%d) - (%d)\n", r, len(lines)))
+		}
+
+		lines = make([]*Pair, 0)
 	}
-
-	covered := 0
-	for _, line := range lines {
-		fmt.Println(line)
-		covered += line[1] - line[0]
-	}
-
-	fmt.Printf("Covered: %d", covered)
-
 }
