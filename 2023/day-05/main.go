5bb6d3c17dacbea3fbca91546b6babc4333c3350
Seth Miller <miller.seth@gmail.com>
2023-12-05 22:44:22 -0600

    2023 Day 5: Part 1

---
+++ 2023/day-05/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func atoi(str string) int {
+	i, _ := strconv.Atoi(str)
+
+	return i
+}
+
+func split(str string) []int {
+	chunks := []int{}
+	for _, str := range strings.Split(str, " ") {
+		if len(str) > 0 {
+			chunks = append(chunks, atoi(str))
+		}
+	}
+
+	return chunks
+}
+
+type Chapter struct {
+	name   string
+	ranges []Range
+}
+
+func (c Chapter) Dest(src int) int {
+	for _, r := range c.ranges {
+		if r.In(src) {
+			return r.Dest(src)
+		}
+	}
+
+	return src
+}
+
+type Range struct {
+	start int
+	end   int
+	dest  int
+}
+
+func (r Range) In(i int) bool {
+	return i >= r.start && i <= r.end
+}
+
+func (r Range) Dest(src int) int {
+	return r.dest + (src - r.start)
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	seeds := split(scanner.Text()[6:])
+	// fmt.Println(seeds)
+	scanner.Scan()
+
+	order := 0
+	chapters := map[int]Chapter{}
+	for scanner.Scan() {
+		header := scanner.Text()
+		names := strings.Split(header[:len(header)-5], "-to-")
+		ch := Chapter{
+			name:   names[1],
+			ranges: []Range{},
+		}
+
+		for scanner.Scan() {
+			values := split(scanner.Text())
+			if len(values) == 0 {
+				break
+			}
+
+			ch.ranges = append(ch.ranges, Range{
+				start: values[1],
+				end:   values[1] + values[2] - 1,
+				dest:  values[0],
+			})
+		}
+
+		chapters[order] = ch
+		// fmt.Println(ch)
+		order++
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		panic(err)
+	}
+
+	min := math.MaxInt
+	for _, seed := range seeds {
+		next := seed
+		for i := 0; i < len(chapters); i++ {
+			ch := chapters[i]
+			next = ch.Dest(next)
+		}
+
+		if next < min {
+			min = next
+		}
+	}
+
+	fmt.Println(min)
+}
--- main.go
+++ main.go
@@ -5,9 +5,5 @@ import (
 )
 
 func main() {
-	fmt.Println("Advent of Code 2022")
-
-	i := []int{1, 2, 3}
-
-	fmt.Println(i[3:])
+	fmt.Println("Advent of Code 2023")
 }
