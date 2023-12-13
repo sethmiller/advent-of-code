05f9d90d128cc928e25febdc5e437f26ad2c2657
Seth Miller <miller.seth@gmail.com>
2023-12-12 22:16:56 -0600

    2023 Day 11: Part 2

--- 2023/day-11/main.go
+++ 2023/day-11/main.go
@@ -35,25 +35,24 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	grid := []string{}
 	var cols []bool
+	rows := []bool{}
 	for scanner.Scan() {
 		line := scanner.Text()
+		grid = append(grid, line)
 
 		if len(cols) == 0 {
 			cols = make([]bool, len(line))
 		}
 
-		grid = append(grid, line)
-		empty := true
+		occupied := false
 		for col, ch := range line {
 			if ch == '#' {
 				cols[col] = true
-				empty = false
+				occupied = true
 			}
 		}
 
-		if empty {
-			grid = append(grid, line)
-		}
+		rows = append(rows, occupied)
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -61,16 +60,24 @@ func main() {
 		panic(err)
 	}
 
+	expansion := 999_999
 	stars := map[Coords]interface{}{}
-	extra := 0
+	extraCols := 0
 	for col, occupied := range cols {
 		if !occupied {
-			extra++
+			extraCols++
+			continue
 		}
 
+		extraRows := 0
 		for row, str := range grid {
+			if !rows[row] {
+				extraRows++
+				continue
+			}
+
 			if str[col] == '#' {
-				co := Coords{r: row, c: col + extra}
+				co := Coords{r: row + (extraRows * expansion), c: col + (extraCols * expansion)}
 				stars[co] = nil
 			}
 		}
@@ -79,17 +86,17 @@ func main() {
 	dists := map[pair]int{}
 	for from := range stars {
 		for to := range stars {
-			if from != to {
+			if _, ok := dists[pair{from: from, to: to}]; !ok && from != to {
 				dists[pair{from: from, to: to}] = from.dist(to)
+				dists[pair{from: to, to: from}] = 0
 			}
 		}
 	}
 
 	sum := 0
-
 	for _, d := range dists {
 		sum += d
 	}
 
-	fmt.Println(sum / 2)
+	fmt.Println(sum)
 }
