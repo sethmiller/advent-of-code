694c17dc5abf56a49d56e70ac686632117c73030
Seth Miller <miller.seth@gmail.com>
2024-12-14 19:47:06 -0600

    2024 Day 3: Part 2

--- 2024/day-03/main.go
+++ 2024/day-03/main.go
@@ -8,20 +8,38 @@ import (
 	"strconv"
 )
 
-var muls = regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+var muls = regexp.MustCompile(`mul\((\d+),(\d+)\)|don't\(\)|do\(\)`)
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	sum := 0
+	on := true
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
 
 		matches := muls.FindAllStringSubmatch(line, -1)
 
 		for _, match := range matches {
+			if match[0] == "do()" {
+				on = true
+				fmt.Println("on")
+				continue
+			}
+			if match[0] == "don't()" {
+				on = false
+				fmt.Println("off")
+				continue
+			}
+
+			if !on {
+				fmt.Printf("skipping %s\n", match[0])
+				continue
+			}
+
 			l, _ := strconv.Atoi(match[1])
 			r, _ := strconv.Atoi(match[2])
+
+			fmt.Printf("%s, %d*%d = %d\n", match[0], l, r, l*r)
 
 			sum += r * l
 		}
