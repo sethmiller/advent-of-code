3d30ec51d954a670ed96d453992e8bd90812ff47
Seth Miller <miller.seth@gmail.com>
2025-01-09 23:13:06 -0600

    2024 Day 12: Part 2

--- 2024/day-12/main.go
+++ 2024/day-12/main.go
@@ -13,6 +13,19 @@ type Node struct {
 	used   bool
 }
 
+type Dir = int
+
+const (
+	North Dir = iota
+	NorthEast
+	East
+	SouthEast
+	South
+	SouthWest
+	West
+	NorthWest
+)
+
 func (from *Node) checkAndAppend(neighbors []*Node, to *Node) []*Node {
 	if from.value == to.value {
 		return append(neighbors, to)
@@ -31,7 +44,7 @@ func (n *Node) neighbors(grid [][]*Node) []*Node {
 		neighbors = n.checkAndAppend(neighbors, grid[n.row-1][n.column])
 	}
 
-	// down
+	// left
 	if n.column > 0 {
 		neighbors = n.checkAndAppend(neighbors, grid[n.row][n.column-1])
 	}
@@ -41,12 +54,128 @@ func (n *Node) neighbors(grid [][]*Node) []*Node {
 		neighbors = n.checkAndAppend(neighbors, grid[n.row][n.column+1])
 	}
 
-	// left
+	// down
 	if n.row < rows-1 {
 		neighbors = n.checkAndAppend(neighbors, grid[n.row+1][n.column])
 	}
 
 	return neighbors
+}
+
+func (n *Node) allNeighbors(grid [][]*Node) map[Dir]*Node {
+	neighbors := map[Dir]*Node{
+		North:     nil,
+		NorthEast: nil,
+		East:      nil,
+		SouthEast: nil,
+		South:     nil,
+		SouthWest: nil,
+		West:      nil,
+		NorthWest: nil,
+	}
+
+	rows := len(grid)
+	columns := len(grid[0])
+
+	// up
+	if n.row > 0 {
+		neighbors[North] = grid[n.row-1][n.column]
+	}
+
+	// left
+	if n.column > 0 {
+		neighbors[West] = grid[n.row][n.column-1]
+	}
+
+	// right
+	if n.column < columns-1 {
+		neighbors[East] = grid[n.row][n.column+1]
+	}
+
+	// down
+	if n.row < rows-1 {
+		neighbors[South] = grid[n.row+1][n.column]
+	}
+
+	// up -> right
+	if n.row > 0 && n.column < columns-1 {
+		neighbors[NorthEast] = grid[n.row-1][n.column+1]
+	}
+
+	// up -> left
+	if n.row > 0 && n.column > 0 {
+		neighbors[NorthWest] = grid[n.row-1][n.column-1]
+	}
+
+	// down -> right
+	if n.row < rows-1 && n.column < columns-1 {
+		neighbors[SouthEast] = grid[n.row+1][n.column+1]
+	}
+
+	// down -> left
+	if n.row < rows-1 && n.column > 0 {
+		neighbors[SouthWest] = grid[n.row+1][n.column-1]
+	}
+
+	return neighbors
+}
+
+func external(a *Node, b *Node, diag *Node, source *Node) bool {
+	return a != nil && b != nil && a.value == source.value && a.value == b.value && a.value != diag.value
+}
+
+func internal(a *Node, b *Node, source *Node) bool {
+	if a == nil && b == nil {
+		return true
+	}
+
+	if a != nil && b == nil && a.value != source.value {
+		return true
+	}
+
+	if a == nil && b != nil && b.value != source.value {
+		return true
+	}
+
+	if a != nil && b != nil && a.value != source.value && b.value != source.value {
+		return true
+	}
+
+	return false
+}
+
+func (n *Node) corners(grid [][]*Node) int {
+	corners := 0
+	a := n.allNeighbors(grid)
+
+	// internal corners
+	if internal(a[North], a[East], n) {
+		corners++
+	}
+	if internal(a[East], a[South], n) {
+		corners++
+	}
+	if internal(a[South], a[West], n) {
+		corners++
+	}
+	if internal(a[West], a[North], n) {
+		corners++
+	}
+
+	// external corners
+	if external(a[North], a[East], a[NorthEast], n) {
+		corners++
+	}
+	if external(a[East], a[South], a[SouthEast], n) {
+		corners++
+	}
+	if external(a[South], a[West], a[SouthWest], n) {
+		corners++
+	}
+	if external(a[West], a[North], a[NorthWest], n) {
+		corners++
+	}
+	return corners
 }
 
 func (n *Node) walls(grid [][]*Node) int {
@@ -109,12 +238,16 @@ func main() {
 
 	total := 0
 	for _, region := range regions {
-		walls := 0
+		corners := 0
+		var value rune
 		for node := range region {
-			walls += node.walls(grid)
+			corners += node.corners(grid)
+			value = node.value
 		}
 
-		total += len(region) * walls
+		fmt.Printf("%c %d * %d\n", value, len(region), corners)
+
+		total += len(region) * corners
 	}
 
 	fmt.Println(len(regions), total)
